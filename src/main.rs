8084e5fee2ac7226da6763c4dcbb43444bb526b4
Tahmid Sadik <tahmidsadik112@gmail.com>
2019-07-13 04:34:16 +0600

    Adds enum for handling meta commands. Also adds a .gitignore file.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,17 @@
 use std::env;
-use std::io::{stdin, stdout, Write };
+use std::io::{stdin, stdout, Write};
+
+enum MetaCommandResult {
+    Success(String),
+    Unknown,
+}
+
+fn handle_commands(cmd: &String) -> MetaCommandResult {
+    match cmd.starts_with(".") {
+        true => MetaCommandResult::Success(cmd.to_owned()),
+        false => MetaCommandResult::Unknown,
+    }
+}
 
 fn main() {
     let args: Vec<String> = env::args().skip(1).collect();
@@ -10,14 +22,20 @@ fn main() {
 
     loop {
         print!("sqlite> ");
-        stdout().flush();
+        stdout().flush().unwrap();
 
-        stdin().read_line(&mut command).expect("Error while trying to read from stdin");
-        if command.trim() == ".exit" {
-            break;
-        }
-        else {
-            println!("Unrecgnized command!")
+        stdin()
+            .read_line(&mut command)
+            .expect("Error while trying to read from stdin");
+
+        match handle_commands(&command.trim().to_owned()) {
+            MetaCommandResult::Success(cmd) => {
+                match cmd.as_ref() {
+                    ".exit" => break,
+                    _ => println!("Unrecognized meta command {}", cmd)
+                }
+            }
+            MetaCommandResult::Unknown => println!("Unrecgnized command!"),
         }
         command = "".to_string();
     }
