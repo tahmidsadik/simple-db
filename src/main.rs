976ce9d14e917603b39edb793e96cc8cf3c2ff77
Tahmid Sadik <tahmidsadik112@gmail.com>
2019-08-03 21:04:39 +0600

    Adds enum to handle separate db commands and meta commands

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,56 @@
 use std::env;
 use std::io::{stdin, stdout, Write};
 
-enum MetaCommandResult {
-    Success(String),
-    Unknown,
+enum MetaCommand {
+    Exit,
+    Unknown(String)
 }
 
-fn handle_commands(cmd: &String) -> MetaCommandResult {
+impl MetaCommand {
+    fn new(command: String) -> MetaCommand {
+        match command.as_ref() {
+            ".exit" => MetaCommand::Exit,
+            _ => MetaCommand::Unknown(command)
+        }
+    }
+}
+
+enum DbCommand {
+    Insert(String),
+    Delete(String),
+    Update(String),
+    Unknown(String)
+}
+
+impl DbCommand {
+    fn new(command: String) -> DbCommand {
+        let v = command.split(" ").collect::<Vec<&str>>();
+        match v[0] {
+            "insert" => DbCommand::Insert(command),
+            "update" => DbCommand::Update(command),
+            "delete" => DbCommand::Delete(command),
+            _ => DbCommand::Unknown(command),
+            
+        }
+    }
+}
+
+enum CommandType {
+    MetaCommand(MetaCommand),
+    DbCommand(DbCommand)
+}
+
+fn handle_commands(cmd: &String) -> CommandType {
     match cmd.starts_with(".") {
-        true => MetaCommandResult::Success(cmd.to_owned()),
-        false => MetaCommandResult::Unknown,
+        true => CommandType::MetaCommand(MetaCommand::new(cmd.to_owned())),
+        false => CommandType::DbCommand(DbCommand::new(cmd.to_owned()))
+    }
+}
+
+fn handle_meta_command(cmd: MetaCommand) {
+    match cmd {
+        MetaCommand::Exit => std::process::exit(0),
+        MetaCommand::Unknown(cmd) => println!("Unrecognized meta command {}", cmd)
     }
 }
 
@@ -27,15 +68,19 @@ fn main() {
         stdin()
             .read_line(&mut command)
             .expect("Error while trying to read from stdin");
-
+            
         match handle_commands(&command.trim().to_owned()) {
-            MetaCommandResult::Success(cmd) => {
-                match cmd.as_ref() {
-                    ".exit" => break,
-                    _ => println!("Unrecognized meta command {}", cmd)
+            CommandType::DbCommand(cmd) => {
+                match cmd {
+                    DbCommand::Insert(ccmd) => println!("Insert Command {}", ccmd),
+                    DbCommand::Update(ccmd) => println!("Update Command {}", ccmd),
+                    DbCommand::Delete(ccmd) => println!("Delete Command {}", ccmd),
+                    DbCommand::Unknown(ccmd) => println!("Unknown Command {}", ccmd),
                 }
             }
-            MetaCommandResult::Unknown => println!("Unrecgnized command!"),
+            CommandType::MetaCommand(cmd) => {
+                handle_meta_command(cmd);
+            }
         }
         command = "".to_string();
     }
