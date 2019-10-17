2842ddb25d02f7afe3591dc83ce36130012083a1
Tahmid Sadik <tahmidsadik112@gmail.com>
2019-10-18 03:16:14 +0600

    Adds table name in error messages in db module.

--- src/database.rs
+++ src/database.rs
@@ -15,14 +15,21 @@ impl Database {
         self.tables.iter().any(|t| t.name == tname)
     }
 
+    pub fn get_table(&self, tname: String) -> &Table {
+        for t in &self.tables {
+            if t.name == tname {
+                return t;
+            }
+        }
+        panic!("Cannot find table, {}", tname);
+    }
+
     pub fn get_table_mut(&mut self, tname: String) -> &mut Table {
         for t in &mut self.tables {
             if t.name == tname {
                 return t;
-            } else {
-                panic!("Cannot find table");
             }
         }
-        panic!("Cannot find table");
+        panic!("Cannot find table, {}", tname);
     }
 }
